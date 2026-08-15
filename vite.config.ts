c3d8f34525f054dddca1d7b865a7e6f6917157ed
amalssaienthusiast <amalssaienthusiast@users.noreply.github.com>
2026-08-15 12:34:03 +0530

    chore: add allowedHosts to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    server: {
+        allowedHosts: [
+            "9d470975-ca12-4c34-95e3-70fccaf7e8a5-00-21yyy0sfxb1cf.sisko.replit.dev",
+        ],
+    },
     plugins: [
         tanstackStart(),
         react(),
